09176ffc919fd453676e2efa09b7f7b7c117e9f0
nath1100 <nath.luu1@gmail.com>
2021-01-12 01:27:24 +1100

    Added texture atlas and main menu buttons

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ if(!PIXI.utils.isWebGLSupported()){
 
 PIXI.utils.sayHello(type);
 
+
 // Create pixi application
 const app = new PIXI.Application({
     width: 1280,
@@ -32,9 +33,12 @@ document.body.appendChild(app.view);
 
 // Load title image and then add it to stage
 app.loader
-    .add("title", "img/title.png")
-    .load((loader, resources) => {
-        const title = new PIXI.Sprite(resources.title.texture);
+.add("img/sheets/sheet.json")
+.load((loader, resources) => {
+        // Aliases
+        let sheet = app.loader.resources["img/sheets/sheet.json"].spritesheet;
+
+        const title = new PIXI.Sprite(sheet.textures["title.png"]);
         
         // Set anchor point and centre title image
         title.anchor.set(0.5);
@@ -62,16 +66,34 @@ app.loader
             // Remove interactivity
             title.interactive = false;
 
-            // Scaling ticker
-            app.ticker.add((delta) => {
+            // Add title disappearance animation to ticker         
+            app.ticker.add(scaleTitle);
+            
+            function scaleTitle(delta) {
                 title.scale.x += 0.02;
                 title.scale.y += 0.02;
                 title.alpha -= 0.02;
+                
+                // Set title to invisible and remove listener when finished disappearing
                 if (title.alpha <= -0.5) {
                     title.visible = false;
-                    app.ticker.remove();
+                    app.ticker.remove(scaleTitle);
                 }
-            });
+            }
+
+            // app.ticker.add((delta) => {
+            //     title.scale.x += 0.02;
+            //     title.scale.y += 0.02;
+            //     title.alpha -= 0.02;
+            //     if (title.alpha <= -0.5) {
+            //         //title.visible = false;
+            //         app.ticker.stop();
+            //     }
+            // });
+
+
+            // Trigger menu enable
+            enableMenu();
         }
 
         // Title scales up when pointer hovers over
@@ -82,9 +104,66 @@ app.loader
             title.scale.set(1);
         }
 
+        // Create menu container
+        const menu = new PIXI.Container();
 
-    })
-    // Load menu screen
-    .load((loader, resources) => {
+        app.stage.addChild(menu);
+
+        // Add menu items to container
+        const sprint_button = new PIXI.Sprite(sheet.textures["sprint_off.png"]);
+        const settings_button = new PIXI.Sprite(sheet.textures["settings_off.png"]);
+
+        // Set menu button positions
+        sprint_button.position.set(0, 0);
+        sprint_button.anchor.set(0.5);
+        settings_button.position.set(0, sprint_button.height + 20);
+        settings_button.anchor.set(0.5);
+
+        // Set interaction options
+        sprint_button
+            .on("mouseover", () => {sprint_button.texture = sheet.textures["sprint_on.png"]})
+            .on("mouseout", () => {sprint_button.texture = sheet.textures["sprint_off.png"]});
+
+        settings_button
+            .on("mouseover", () => {settings_button.texture = sheet.textures["settings_on.png"]})
+            .on("mouseout", () => {settings_button.texture = sheet.textures["settings_off.png"]});
+
+        // Add menu children
+        menu
+        .addChild(
+            sprint_button,
+            settings_button
+        );
         
+        // Set initial position (off-screen + buffer) and properties of menu container
+        menu.position.set(app.screen.width + menu.width + 50, app.screen.height / 2);
+        menu.alpha = 0;
+        menu.anchor = 0.5;
+        
+        function enableMenu() {
+            // Slide menu into view whilst increasing visibility
+            app.ticker.add(menuIn);
+            
+            // Animation for opening main menu
+            function menuIn(delta) {
+                menu.alpha += 0.5;
+                menu.x -= 40;
+                // If menu reaches centre, stop moving
+                if (menu.x - 40 <= app.screen.width / 2) {
+                    menu.x = app.screen.width / 2;
+                    // Set alpha to 1 if not already
+                    menu.alpha = 1;
+                    
+                    // Enable menu interactivity
+                    for (var item of menu.children) {
+                        item.interactive = true;
+                        item.buttonMode = true;
+                    }
+
+                    // Remove this listener from the ticker
+                    app.ticker.remove(menuIn);
+                }
+            }
+        }
+
     });
